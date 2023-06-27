e3ad195ce9004dc620e84d14d87b0ccec9148816
Christine <christinekhekama99@gmail.com>
2023-06-27 17:41:00 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  const url = 'https://anapioficeandfire.com/api/books';
-  return fetch (url);
+  return fetch( 'https://anapioficeandfire.com/api/books') 
+  .then(response => response.json())
+  .then(data => {
+    renderBooks(data);
+});
+  
 
  
 }
